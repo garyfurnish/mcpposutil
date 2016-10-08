ca041c120db1d48e4b70c19027dd8d9e38aa7e5a
Gary Furnish <gfurnish@gfurnish.net>
2016-10-08 17:43:20 -0600

    Stub functions for windows.

--- mcpposutil/mcpposutil/include/mcpposutil/mcpposutil/memory_sections.hpp
+++ mcpposutil/mcpposutil/include/mcpposutil/mcpposutil/memory_sections.hpp
@@ -4,6 +4,10 @@
 #include "memory_sections_linux.hpp"
 #elif defined(__APPLE__)
 #include "memory_sections_osx.hpp"
+#elif defined(_WIN32)
+inline auto get_data_start() -> void * { return nullptr; }
+inline auto get_data_end() -> void * { return nullptr; }
+inline void print_pmap() {}
 #else
 #error "UNKNOWN PLATFORM"
 #endif
